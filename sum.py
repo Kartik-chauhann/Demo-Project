f5aa0ef8fff07d24db7163fa1dcbb853d1e6a8b0
Kartik-chauhann <chauhankartik8893@gmail.com>
2026-08-04 21:55:49 +0530

    some change

--- sum.py
+++ sum.py
@@ -3,4 +3,5 @@ num1=int(input())
 print("enter the number 2")
 num2=int(input()) 
 num3=num1+num2
-print("the sum of two numbers is",num3)  
+print("the sum of two numbers is",num3)
+print("hello world")  
